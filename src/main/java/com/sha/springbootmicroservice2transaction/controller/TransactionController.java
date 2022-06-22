0af8ffa5da95c4791446c719750fa5e09e857135
Oğuzhan Akın <oguzhanakin.a@gmail.com>
2022-06-22 20:40:27 +0300

    initial

---
+++ src/main/java/com/sha/springbootmicroservice2transaction/controller/TransactionController.java
@@ -0,0 +1,38 @@
+package com.sha.springbootmicroservice2transaction.controller;
+
+import com.sha.springbootmicroservice2transaction.model.Transaction;
+import com.sha.springbootmicroservice2transaction.service.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("rest/transaction")
+public class TransactionController
+{
+    @Autowired
+    private ITransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<?> saveTransaction(@RequestBody Transaction transaction)
+    {
+        return new ResponseEntity<>(transactionService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{transactionId}")
+    public ResponseEntity<?> deleteTransaction(@PathVariable Long transactionId)
+    {
+        transactionService.deleteTransaction(transactionId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<List<Transaction>> findAllTransactionsOfUser(@PathVariable Long userId)
+    {
+        return ResponseEntity.ok(transactionService.findAllTransactionsOfUser(userId));
+    }
+}
